f83863d910ce5810039327ebba5bad19efb003cd
InterstellarOne <isaacizen@proton.me>
2026-08-25 11:19:38 -0700

    Added more startpages and added proprietary and safari props

---
+++ src/content.config.ts
@@ -0,0 +1,26 @@
+import { defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
+import { z } from 'astro/zod';
+
+const startpages = defineCollection({
+	// Load json files in the `src/content/startpages/` directory.
+	loader: glob({ base: './src/content/startpages', pattern: '**/*.json' }),
+	// Type-check frontmatter using a schema
+	schema: ({ }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+            firefoxLink: z.url().optional(),
+            chromeLink: z.url().optional(),
+            gitLink: z.url().optional(),
+            websiteLink: z.url().optional(),
+			safariLink: z.url().optional(),
+			dateUpdated: z.number(),
+            image: z.object({src: z.url()}),
+            tags: z.array(z.string()),
+            dateAdded: z.number(),
+            proprietary: z.boolean().optional(),
+		}),
+});
+
+export const collections = { startpages };
